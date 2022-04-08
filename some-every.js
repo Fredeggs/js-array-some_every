af3e5120249bbd11edbfd70f9401e92247bb0910
Fredeggs <42360605+Fredeggs@users.noreply.github.com>
2022-04-08 11:41:50 -0400

    Add files via upload
    Completed Assignment

--- some-every.js
+++ some-every.js
@@ -36,13 +36,11 @@ Examples:
 */
 
 function hasOnlyOddNumbers(arr) {
-    arr.some(function(val){
-        if(!val%2){
-            return false; 
-        } 
-        return true;
-    })
+    return arr.every(function(val){
+        return val%2;
+    });
 }
+
 
 /*
 Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
@@ -73,7 +71,11 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+    return arr.every(function(obj){
+        return (key in obj);
+    })
+}
 
 /*
 Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
@@ -91,4 +93,8 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+    return arr.every(function(obj){
+        return obj[key] === searchValue;
+    })
+}
